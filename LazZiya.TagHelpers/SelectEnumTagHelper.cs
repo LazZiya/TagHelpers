115eb2e23585246cfe598584b14c47022c2dc90f
Ziya Mollamahmut <z_manllamahmoud@hotmail.com>
2019-02-10 16:23:56 +0300

    added SelectEnumTagHelper

---
+++ LazZiya.TagHelpers/SelectEnumTagHelper.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace LazZiya.TagHelpers
+{
+    public class SelectEnumTagHelper : TagHelper
+    {
+        /// <summary>
+        /// (int)MyEnum.ValueName
+        /// </summary>
+        public int SelectedValue { get; set; }
+
+        /// <summary>
+        /// typeof(MyEnum)
+        /// </summary>
+        public Type EnumType { get; set; }
+
+        /// <summary>
+        /// localization method as delegate
+        /// e.g. delegate(string s) { return _localizaer["My value"]; }
+        /// </summary>
+        public Func<string, string> TextLocalizerDelegate { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "select";
+
+            foreach (int e in Enum.GetValues(EnumType))
+            {
+                var op = new TagBuilder("option");
+
+                op.Attributes.Add("value", $"{e}");
+
+                var displayText = TextLocalizerDelegate == null 
+                    ? GetEnumFieldDisplayName(e)
+                    : GetEnumFieldLocalizedDisplayName(e);
+
+                op.InnerHtml.Append(displayText);
+
+                if (e == SelectedValue)
+                    op.Attributes.Add("selected", "selected");
+
+                output.Content.AppendHtml(op);
+            }
+        }
+
+        private string GetEnumFieldDisplayName(int value)
+        {
+            // get enum field name
+            var fieldName = EnumType.GetEnumName(value);
+
+            //get Display(Name = "Field name")
+            var displayName = EnumType.GetField(fieldName).GetCustomAttributes(false).OfType<DisplayAttribute>().SingleOrDefault()?.Name;
+
+            return displayName ?? fieldName;
+        }
+
+        private string GetEnumFieldLocalizedDisplayName(int value)
+        {
+            var text = GetEnumFieldDisplayName(value);
+
+            return TextLocalizerDelegate(text);
+        }
+    }
+}
