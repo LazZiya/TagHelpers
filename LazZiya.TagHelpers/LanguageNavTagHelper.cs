6a4c27ec6a0c3b4733656e88fcc238539488bf6b
Ziya Mollamahmut <ziyadmm@gmail.com>
2019-04-04 18:34:59 +0300

    todo: language navigation

---
+++ LazZiya.TagHelpers/LanguageNavTagHelper.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace LazZiya.TagHelpers
+{
+    public class LanguageNavTagHelper : TagHelper
+    {   
+        /// <summary>
+        /// string list of supported cultures list seperated by comma
+        /// ar,tr,en
+        /// </summary>
+        public string SupportedCultures { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            base.Process(context, output);
+        }
+    }
+}
