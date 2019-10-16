403698bf504858c47760c7cc28613ea9af6787d3
Ziya Mollamahmut <ziyadmm@gmail.com>
2019-10-16 15:45:06 +0300

    ...

--- LazZiya.TagHelpers/Properties/Resources.Designer.cs
+++ LazZiya.TagHelpers/Properties/Resources.Designer.cs
@@ -10,7 +10,6 @@
 
 namespace LazZiya.TagHelpers.Properties {
     using System;
-    using System.Reflection;
     
     
     /// <summary>
@@ -40,7 +39,7 @@ namespace LazZiya.TagHelpers.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("LazZiya.TagHelpers.Properties.Resources", typeof(Resources).GetTypeInfo().Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("LazZiya.TagHelpers.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
